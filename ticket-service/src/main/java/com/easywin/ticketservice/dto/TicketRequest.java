5c87ed876d80d11ba0ce5aea276ecf2a010a0f93
patrykkondrat <01150244@pw.edu.pl>
2022-12-24 23:53:32 +0100

    wallet works

--- ticket-service/src/main/java/com/easywin/ticketservice/dto/TicketRequest.java
+++ ticket-service/src/main/java/com/easywin/ticketservice/dto/TicketRequest.java
@@ -2,12 +2,14 @@ package com.easywin.ticketservice.dto;
 
 import com.easywin.ticketservice.model.TicketLineItems;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class TicketRequest {
