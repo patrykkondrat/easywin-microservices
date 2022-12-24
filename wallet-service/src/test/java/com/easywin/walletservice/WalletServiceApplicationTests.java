a9c322867c4091b513335ac8fb63d20ce30e9cf9
patrykkondrat <01150244@pw.edu.pl>
2022-12-24 19:59:06 +0100

    something wrong with tests, trying to fix it

--- wallet-service/src/test/java/com/easywin/walletservice/WalletServiceApplicationTests.java
+++ wallet-service/src/test/java/com/easywin/walletservice/WalletServiceApplicationTests.java
@@ -1,13 +1,72 @@
 package com.easywin.walletservice;
 
+import com.easywin.walletservice.dto.WalletRequest;
+import com.easywin.walletservice.repository.WalletRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class WalletServiceApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	ObjectMapper objectMapper;
+
+	@Autowired
+	private WalletRepository walletRepository;
+
+
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void getBalance() throws Exception {
+		WalletRequest walletRequest = getWalletRequest();
+
+		String requestBody = objectMapper.writeValueAsString(walletRequest);
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/wallet")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(requestBody))
+				.andExpect(status().isOk())
+				.andExpect((ResultMatcher) content().string("{\"id\":1,\"balance\":100.0}"));
+	}
+
+	@Test
+	void increaseBallance() throws Exception {
+		String walletString = objectMapper.writeValueAsString(getWalletRequest());
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/wallet/-100.0")
+						.param("id", "1"))
+					.andExpect(status().isOk());
+	}
+
+
+	private WalletRequest getWalletRequest() {
+		return WalletRequest.builder()
+				.id("2418643175")
+				.build();
 	}
 
 }
