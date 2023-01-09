b46c6f72cddf96ca542f42b5067474f8dd6df5d0
patrykkondrat <01150244@pw.edu.pl>
2023-01-09 13:54:51 +0100

    change secure discovery to Security Filter Chain

--- discovery-server/src/main/java/com/easywin/discoveryserver/config/SecrurityConfig.java
+++ discovery-server/src/main/java/com/easywin/discoveryserver/config/SecrurityConfig.java
@@ -1,43 +1,44 @@
 package com.easywin.discoveryserver.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-
-/*
-TODO
-Secure on Security filter chain not WebSecurityConfigurerAdapter
- */
 
 @Configuration
 @EnableWebSecurity
-public class SecrurityConfig extends WebSecurityConfigurerAdapter {
+public class SecrurityConfig {
 
     @Value("${eureka.username}")
     private String username;
     @Value("${eureka.password}")
     private String password;
 
-
-    @Override
-    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder.inMemoryAuthentication()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .withUser(username).password(password)
-                .authorities("USER");
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserBuilder users = User.withDefaultPasswordEncoder();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(users.username(username).password(password).roles("ADMIN").build());
+        return manager;
     }
-
-    @Override
-    public void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable()
-                .authorizeRequests().anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
+    @Bean
+    @Order(1)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeRequests(autorize -> autorize
+                .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
     }
 }
