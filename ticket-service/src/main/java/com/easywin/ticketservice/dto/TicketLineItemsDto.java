418e4ff84effa362e5271e6568139d0582ddca06
patrykkondrat <01150244@pw.edu.pl>
2023-01-25 23:09:52 +0100

    ticket check is valid bet adding

--- ticket-service/src/main/java/com/easywin/ticketservice/dto/TicketLineItemsDto.java
+++ ticket-service/src/main/java/com/easywin/ticketservice/dto/TicketLineItemsDto.java
@@ -1,15 +1,13 @@
 package com.easywin.ticketservice.dto;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class TicketLineItemsDto {
     private Long _id;
     private String betId;
