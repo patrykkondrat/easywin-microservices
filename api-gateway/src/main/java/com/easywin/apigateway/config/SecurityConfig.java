367ab855c8506f6a16fabbf26ab755a7fa33f7da
patrykkondrat <01150244@pw.edu.pl>
2023-02-16 22:50:59 +0100

    api-gateway: Specify routes for SecurityConfig

--- api-gateway/src/main/java/com/easywin/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/easywin/apigateway/config/SecurityConfig.java
@@ -15,8 +15,17 @@ public class SecurityConfig {
         serverHttpSecurity
                 .csrf().disable()
                 .authorizeExchange(exchange ->
-                        exchange.pathMatchers("/eureka/**", "/api/bet/**") //unauthorize /api/bet/add
+                        exchange.pathMatchers("/api/bet", "/api/bet/id", "/api/bet/discipline/**",
+                                        "/api/event", "/api/event/id")
                                 .permitAll()
+                                .pathMatchers("/chat", "/api/ticket", "api/wallet/id")
+                                .hasAnyRole("ROLE_USER", "ROLE_STAFF", "ROLE_ADMIN")
+                                .pathMatchers("/api/bet/create", "/api/bet/isbet", "/api/event/create",
+                                        "/api/event/publish", "/api/event/update", "/api/event/delete")
+                                .hasAnyRole("ROLE_STAFF", "ROLE_ADMIN")
+                                .pathMatchers("/api/bet/delete", "/eureka/**", "/api/event/publish",
+                                        "/api/wallet/add", "/api/wallet")
+                                .hasAnyRole("ROLE_ADMIN")
                                 .anyExchange()
                                 .authenticated())
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
