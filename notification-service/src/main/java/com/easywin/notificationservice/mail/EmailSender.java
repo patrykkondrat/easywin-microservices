e2bc5dcdb1d33100ae05db9b8dde3a6ef6702902
patrykkondrat <01150244@pw.edu.pl>
2023-03-20 04:24:24 +0100

    notification-service: handler MessageException

--- notification-service/src/main/java/com/easywin/notificationservice/mail/EmailSender.java
+++ notification-service/src/main/java/com/easywin/notificationservice/mail/EmailSender.java
@@ -1,8 +1,7 @@
 package com.easywin.notificationservice.mail;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -12,18 +11,32 @@ import javax.mail.internet.MimeMessage;
 import java.nio.charset.StandardCharsets;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class EmailSender {
 
-    @Autowired
     private JavaMailSender mailSender;
+
+    public EmailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
     public void sendMail(String to, String subject, String text)
             throws MessagingException {
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name());
-        helper.setTo(to);
-        helper.setSubject(subject);
-        helper.setText(text, true);
-        mailSender.send(message);
+        try {
+            if (mailSender == null) {
+                throw new IllegalStateException("mailSender is null");
+            }
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name());
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text, true);
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            log.error("Error while sending email - {}", e.getMessage());
+            throw new RuntimeException("Error while sending email", e);
+        }
+
     }
 }
