c7c9374559ae89ba0eff86e58c72c09f530f882a
patrykkondrat <01150244@pw.edu.pl>
2023-03-21 16:00:20 +0100

    promo-service: some publish event tests

--- promo-service/src/test/java/com/easywin/promoservice/service/EventServiceTest.java
+++ promo-service/src/test/java/com/easywin/promoservice/service/EventServiceTest.java
@@ -1,22 +1,25 @@
 package com.easywin.promoservice.service;
 
+import com.easywin.promoservice.dto.EventRequest;
 import com.easywin.promoservice.dto.EventResponse;
 import com.easywin.promoservice.event.PromoEvent;
 import com.easywin.promoservice.model.Event;
 import com.easywin.promoservice.model.EventStatus;
 import com.easywin.promoservice.repository.EventRepository;
-import org.apache.kafka.common.protocol.types.Field;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -84,18 +87,56 @@ class EventServiceTest {
 
     @Test
     void createEvent() {
+        EventRequest eventRequest = new EventRequest("Test1", "Test1",
+                "Test1", "Test1", "Test1", "Test1", "Test1","Test1");
+
+        Event event = this.generateEvent("1");
+
+        when(eventRepository.save(any(Event.class))).thenReturn(event);
+
+        eventService.createEvent(eventRequest);
+
+        verify(eventRepository, times(1)).save(any(Event.class));
     }
 
     @Test
     void updateEvent() {
+        Event event = this.generateEvent("2");
+
+        EventRequest eventRequest = new EventRequest();
+        eventRequest.setName("Test2");
+        eventRequest.setDescription("Test2");
+        eventRequest.setStartDate("Test2");
+        eventRequest.setEndDate("ThisWillChange");
+        eventRequest.setStartTime("Test2");
+        eventRequest.setEndTime("ThisWillChangeToo");
+        eventRequest.setLocation("Test2");
+        eventRequest.setOrganizer("Test2");
+
+        when(eventRepository.findById("2")).thenReturn(java.util.Optional.of(event));
+        when(eventRepository.save(any(Event.class))).thenReturn(event);
+
+        eventService.updateEvent(eventRequest, "2");
+
+        verify(eventRepository, times(1)).save(any(Event.class));
+
     }
 
     @Test
     void publishEvents() {
-    }
+        Event event1 = this.generateEvent("4");
+        Event event2 = this.generateEvent("5");
+        List<String> ids = List.of("4", "5");
 
-    @Test
-    void mapToDto() {
+        when(eventRepository.findById("4")).thenReturn(Optional.of(event1));
+        when(eventRepository.findById("5")).thenReturn(Optional.of(event2));
+
+        eventService.publishEvents(ids);
+
+        verify(kafkaTemplate, times(2)).send(anyString(), any(PromoEvent.class));
+        verify(eventRepository, times(2)).save(any(Event.class));
+        assertEquals(EventStatus.PUBLISHED, event1.getStatus());
+        assertEquals(EventStatus.PUBLISHED, event2.getStatus());
     }
 
     private Event generateEvent(String suffix) {
